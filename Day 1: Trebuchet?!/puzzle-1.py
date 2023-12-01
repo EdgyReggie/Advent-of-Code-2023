0128192ce4eceeac1be66075a95a4f630bb6e23c
EdgyReggie <reatho00@gmail.com>
2023-12-01 23:13:08 +0800

    fixed fundamental problem + comments

--- Day 1: Trebuchet?!/puzzle-1.py
+++ Day 1: Trebuchet?!/puzzle-1.py
@@ -1,16 +1,32 @@
+# Open the file, read each line and check every character if its a number and add it to a list.
 with open('input') as f:
     lines = []
     for line in f.readlines():
-        numChars = []
+        num_chars = []
         for chars in line: 
             if chars.isdigit():
-                numChars.append(chars)
-        lines.append(numChars)
+                num_chars.append(chars)
+        lines.append(num_chars)
     
-    nums = []
-    
+    # For each of the arrays in the list, get the first and last values and append the to the list.
+    # This however does create a list of tuples.
+    tup_nums = []
     for array in lines:
-        numbers = ''.join(map(str, array))
-        nums.append(numbers)
+        if len(array) > 1: 
+            first_num = array[0]
+            last_num = array[-1]
+            tup_nums.append((first_num, last_num))
+        elif len(array) == 1:
+            first_num = array[0]
+            last_num = array[0]
+            tup_nums.append((first_num, last_num)) 
 
-    print(nums)
+    # For each tuple, use the map function to convert the 2 numbers to a string and convert the string
+    # into an int.
+    result = []
+    for tup in tup_nums:
+        value = int(''.join(map(str, tup)))
+        result.append(value)    
+        
+    # Print the sum of all the numbers
+    print(sum(result))
