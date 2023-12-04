8f8e77a8a3f459c279d1fc28bbe81db75e882c03
EdgyReggie <reatho00@gmail.com>
2023-12-04 22:05:22 +0800

    began working on day 3

---
+++ Day 3: Gear Ratios/part-1.py
@@ -0,0 +1,8 @@
+def main():
+    with open("example-input", "r") as f:
+        for line in f:
+            for char in line:
+                print("I am still working on this")                            
+
+if __name__ == "__main__":
+    main()
