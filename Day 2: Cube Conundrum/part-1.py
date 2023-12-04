dcbbff19dd768c8900f69692bd9c586bb7a702e2
EdgyReggie <reatho00@gmail.com>
2023-12-04 16:33:05 +0800

    fixed solution working

--- Day 2: Cube Conundrum/part-1.py
+++ Day 2: Cube Conundrum/part-1.py
@@ -2,25 +2,21 @@ def main():
     games = []
     possible_games = []
 
-    result = 0
-
     with open("input", "r") as f: 
         for line in f: 
-            game_info = line.strip().split(";")
-            
-            # Starting at index one, strip each subset of white space,
-            # split by comma, and use list comprehension to create a 
-            # new list of lists contaning the colour and count. 
-            subsets = [subset.strip().split(",") for subset in game_info[1:]]
-            games.append(subsets)
+            trim = line.split(":")
+            game_info = [hand.strip().split(";") for hand in trim[1:]]
+
+            for game in game_info:
+                hands = [subset.strip().split(",") for subset in game]
+                games.append(hands)
 
     # This is just a fancy for loop that keeps track of the element and the index
     for i, game in enumerate(games, start=1):
-        if possibile_game(game):
-            possible_games.append(i)
-
+       if possibile_game(game):
+           possible_games.append(i)
+            
     print(sum(possible_games))
-
 
 def possibile_game(game):
     max_red = 12
@@ -38,7 +34,6 @@ def possibile_game(game):
             elif cube == "blue" and count > max_blue:
                 return False
     return True
-
 
 if __name__ == "__main__":
     main()
