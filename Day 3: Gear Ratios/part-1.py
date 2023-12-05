e5d56d5676421dfc1ba2dc5df587c2cae6d99406
EdgyReggie <reatho00@gmail.com>
2023-12-05 12:41:46 +0800

    file input + 2D array allocatin

--- Day 3: Gear Ratios/part-1.py
+++ Day 3: Gear Ratios/part-1.py
@@ -1,8 +1,29 @@
+from queue import Queue
+import string
+
 def main():
+    queue = Queue() 
+
+    # Thank you Chat-GPT 
     with open("example-input", "r") as f:
-        for line in f:
-            for char in line:
-                print("I am still working on this")                            
+           lines = f.readlines() 
+
+    # You could probably do this without making everyline the same length
+    max_length = max(len(line) for line in lines)
+    # adjust the input so that every line is the same length, no sizing issues between the rows 
+    schematic = [list(line.strip().ljust(max_length)) for line in lines]
+
+    for row in schematic:
+        for char in row:
+            if char.isdigit():
+                queue.put(char)
+            elif char == '.':
+                print()
+            elif is_special_char(char):
+                print()
+                
+def is_special_char(char):
+    return char.isprintable() and not char.isalnum() and char is not string.whitespace
 
 if __name__ == "__main__":
     main()
