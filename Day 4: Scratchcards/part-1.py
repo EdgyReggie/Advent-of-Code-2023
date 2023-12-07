0c570c190f31dac3e9c767860b416fe573e93739
EdgyReggie <reatho00@gmail.com>
2023-12-07 20:15:20 +0800

    added file read and parsing

--- Day 4: Scratchcards/part-1.py
+++ Day 4: Scratchcards/part-1.py
@@ -1,6 +1,13 @@
 def main():
+    game_numbers = []
+    total_numbers = []
     with open("example-input", "r") as f:
-        print() 
+        for line in f: 
+            game = line.split(":")
+            game_numbers = [number.strip().split("|") for number in game[1:]]
+            total_numbers.append(game_numbers)
+
+    print(total_numbers)
 
 if __name__ == "__main__":
     main()
