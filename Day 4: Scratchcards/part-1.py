ea69266503b8072f235e4ade738de92151597a53
EdgyReggie <reatho00@gmail.com>
2023-12-08 17:15:47 +0800

    fixed parsing bug

--- Day 4: Scratchcards/part-1.py
+++ Day 4: Scratchcards/part-1.py
@@ -1,23 +1,28 @@
 def main():
     game_numbers = []
     all_games = []
+
     with open("example-input", "r") as f:
         for line in f: 
             game = line.split(":")
-            game_numbers = [int(number.strip().split("|")) for number in game[1:]]
+            game_numbers = [number.strip().split("|") for number in game[1:]]
+            for game_set in game_numbers:
+                for indv_num in game_set:
+                    nums = indv_num.split(" ")
+                    c = nums.count("")
+                    for i in range(c):
+                        nums.remove("")
+                    print(nums)
             all_games.append(game_numbers)
     
     results = []
 
     for game in all_games:
         for game_values in game:
-            winning_nums = game_values[0]
+            winning_nums = [int(num) for num in game_values[0] if num.strip()]
             player_nums = game_values[1]
             for num in winning_nums:
-           #     results[num] = player_nums.count(num)
-                print()
-
-    print(type(all_games[0]))
+                results[num] = player_nums.count(num)
 
 if __name__ == "__main__":
     main()
