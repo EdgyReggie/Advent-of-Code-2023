ff001e0679fc959dbb9b11ede2cf425cb1685abb
EdgyReggie <reatho00@gmail.com>
2023-12-09 19:03:31 +0800

    fixed parsing problem

--- Day 4: Scratchcards/part-1.py
+++ Day 4: Scratchcards/part-1.py
@@ -7,22 +7,25 @@ def main():
             game = line.split(":")
             game_numbers = [number.strip().split("|") for number in game[1:]]
             for game_set in game_numbers:
+                working_list = []
                 for indv_num in game_set:
                     nums = indv_num.split(" ")
                     c = nums.count("")
                     for i in range(c):
                         nums.remove("")
-                    print(nums)
-            all_games.append(game_numbers)
-    
+                    working_list.append(nums)
+                all_games.append(working_list)
+
+    print(all_games)
+
     results = []
 
     for game in all_games:
         for game_values in game:
-            winning_nums = [int(num) for num in game_values[0] if num.strip()]
+            winning_nums = game_values[0]
             player_nums = game_values[1]
-            for num in winning_nums:
-                results[num] = player_nums.count(num)
+            #for num in winning_nums:
+            #    results[num] = player_nums.count(num)
 
 if __name__ == "__main__":
     main()
